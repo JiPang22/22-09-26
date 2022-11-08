daeab384a62f85f99795eebbdb00eb0a9a898bf6
dragon <wlvud6902@gmail.com>
2022-11-08 14:19:21 +0900

    a

--- a.f90
+++ a.f90
@@ -1,22 +1,23 @@
 program a
 implicit none
 integer*4 i,j,interval
-real*4 eta,gam,om,alpha,deta,etadot,detadot;interval
+real*4 gam,om,alpha,eta,deta,etadot,detadot,dtau
 open(6,file='aa')
 
 do j=0,10
-gam=j*0.1;eta=0.01;dtau=0.01;om=1.;alpha=1.etadot=0.01;interva=100
+gam=j*0.1
+eta=0.01;dtau=0.01;om=2.;alpha=1.;etadot=0.01;interval=600
 
 do i=0,interval
 write(6,*) i*dtau,eta
-detadot=-gam*etadot-om**2 *eta+0.;deta=etadot
+detadot=-2*gam*etadot-om**2*eta+alpha;deta=etadot
 etadot=etadot+detadot*dtau;eta=eta+deta*dtau
 enddo
 
 interval=interval+1
-do i=interval,1000
+do i=interval,2000
 write(6,*) i*dtau,eta
-detadot=-gam*etadot-om**2 *eta+alpha;deta=etadot
+detadot=-2*gam*etadot-om**2*eta;deta=etadot
 etadot=etadot+detadot*dtau;eta=eta+deta*dtau
 enddo
 
